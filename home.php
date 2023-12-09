29a8e3657bf2c41ed4924730c7a5b21d82533777
SaimaAhmed1 <108589596+SaimaAhmed1@users.noreply.github.com>
2023-12-10 00:43:48 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,59 @@
+<?php
+echo "Hellow world <br>";
+
+// variable
+$name="pritilota<br>";
+// changeable
+ echo $name;
+//  not changeable/string literal
+echo 'I am $name <br>';
+
+// concat with single quotations
+ $name="pritilota";
+ $age=23;
+ $hobby="Reading Books";
+ echo 'I am'.$name.'.I am'.$age.'years old <br>';
+// without concat double quotations
+echo "I am $name.I am $age years old and I love $hobby.<br>";
+
+// better commentS
+// *success
+//!danger
+// TODO:
+/**
+ * MULTI COMMENTS
+ * 
+ */
+
+
+//  LAST VALUE ALWAYS APPROVED
+$name='Hurvey';
+$name='kamal';
+echo "$name <br>";
+// last value not change
+$name='hurvey';
+echo "$name <br>";
+$name='kamal';
+
+
+// define
+define('PI',3.1416);
+echo PI;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
